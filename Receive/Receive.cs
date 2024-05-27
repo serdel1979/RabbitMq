e7b086094f5129e44f4b1ae6d94a09357b084e4a
sergio <sdlbsso@gmail.com>
2024-05-27 11:46:53 -0300

    ejemplo básico de envío de mensajes

---
+++ Receive/Receive.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    //establecer canal
+    using (var chanel = connection.CreateModel())
+    {
+        /*
+        Acá adentro del canal, es donde ocurre todo.
+        Acá creamos las colas
+        */
+        chanel.QueueDeclare(queue: "hola", durable: false,
+            exclusive: false, autoDelete: false, arguments: null);
+
+        var consumer = new EventingBasicConsumer(chanel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+
+            Console.WriteLine($"[x] Received {message}");
+        };
+
+        chanel.BasicConsume(queue: "hola", autoAck: true, consumer: consumer);
+
+        Console.WriteLine($"Press any key to exit ...");
+
+
+        Console.ReadLine();
+    }
+
+
+
+}
---
+++ Send/Send.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    //establecer canal
+    using (var chanel = connection.CreateModel())
+    {
+         /*
+         Acá adentro del canal, es donde ocurre todo.
+         Acá creamos las colas
+         */
+        chanel.QueueDeclare(queue: "hola", durable: false,
+            exclusive: false, autoDelete: false, arguments: null);
+        string message = "Hola soy morgan";
+
+        //PARA TRANSMITIR AL CANAL, PASO EL STRING A BYTE
+        var body = Encoding.UTF8.GetBytes(message);
+
+        chanel.BasicPublish(exchange: "", routingKey: "hola", basicProperties: null, body: body);
+
+        Console.WriteLine($"[x] Sent {message}");
+    
+    }
+
+    Console.WriteLine($"Press any key to exit ...");
+
+    Console.ReadLine();
+
+}
